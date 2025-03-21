f08abfb3225bfeacc6c8314c08bb22e6b86cfa69
1t0s <tliberty@protonmail.com>
2025-03-21 15:51:20 +0100

    Documented loss functions

--- loss_functions.py
+++ loss_functions.py
@@ -3,11 +3,23 @@ import torch.nn.functional as F
 
 
 def compute_content_loss(content_tensor, generated_tensor):
+    """Computes MSE loss between tensors from content and generated layers.
+
+       Args:
+           content_tensor: torch.tensor, output of vgg19 content layer for content image.
+           generated_tensor: torch.tensor, output of vgg19 content layer for image that is being generated.
+    """
     content_loss = F.mse_loss(content_tensor, generated_tensor)
     return content_loss
 
 
 def compute_style_loss(style_features, generated_features):
+    """Computes MSE loss between gram matrices of style features and generated features.
+
+       Args:
+           style_features: dict{layer, tensor}, output of vgg19 style layers for style image.
+           generated_features: dict{layer, tensor}, output of vgg19 style layers for image that is being generated.
+    """
     style_loss = 0
     for layer in style_features:
         target_gram = _gram_matrix(style_features[layer])
@@ -17,6 +29,7 @@ def compute_style_loss(style_features, generated_features):
 
 
 def _gram_matrix(tensor):
+    """Compute gram matrix G = A * A^T, which represents the style texture, for given tensor"""
     a, b, c, d = tensor.size()
     features = tensor.view(a * b, c * d)
     gram = torch.mm(features, features.t())
